fc8d7b1f4d5445a4513600ed1ed30a4dfc1fb7b2
Karin <102821746+karincaceres@users.noreply.github.com>
2026-03-27 20:11:44 -0300

    Add files via upload

--- src/Pages/Cargando.js
+++ src/Pages/Cargando.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import './Main.css';
+import React from "react";
+import "./Main.css";
 
-import FondoMobile from '../assets/Cargando/Cargando-vertical.png';
-import FondoDesktop from '../assets/Cargando/Cargando-horizontal.png';
+import FondoMobile from "../assets/Cargando/Cargando-vertical.png";
+import FondoDesktop from "../assets/Cargando/Cargando-horizontal.png";
 // eslint-disable-next-line
-import BotonEntradas from '../assets/Cargando/bot_2.png';
+import BotonEntradas from "../assets/Cargando/bot_2.png";
 // eslint-disable-next-line
-import BotonEntradasPC from '../assets/Cargando/bot_2_pc.png';
+import BotonEntradasPC from "../assets/Cargando/bot_2_pc.png";
 import BotonIG_cel from "../assets/Cargando/btn_ig.png";
 import BotonIG_pc from "../assets/Cargando/btn_ig.png";
 import botontiktok from "../assets/Cargando/tk.png";
+import backBotton from "../assets/Cargando/fondo_bot.png";
+
 class Cargando extends React.Component {
   render() {
-    const min = window.innerWidth >= 790; // breakpoint desktop
+    const min = window.innerWidth >= 790;
     const backImg = min ? FondoDesktop : FondoMobile;
     // eslint-disable-next-line
     const botonEntradas = min ? BotonEntradasPC : BotonEntradas;
@@ -31,23 +33,33 @@ class Cargando extends React.Component {
           justifyContent: "center",
           alignItems: "center",
           position: "relative",
+          overflow: "hidden",
         }}
       >
-        {/* Frase + logo IG */}
         <div
           style={{
             position: "absolute",
-            bottom: min ? "32%" : "20%",
-            left: min ? "73%" : "50%",
-            transform: "translateX(-50%)",
+            bottom: min ? "20%" : "20%",
             display: "flex",
-            flexDirection: min ? "row" : "row", // cambia disposición según dispositivo
+            flexDirection: "row",
             alignItems: "center",
             justifyContent: "center",
-            gap: min ? "6px" : "3px",
+            gap: min ? "20px" : "8px",
             textAlign: "center",
+            width: min ? "900px" : "390px",
           }}
         >
+          <img
+            src={backBotton}
+            alt=""
+            style={{
+              position: "absolute",
+              width: min ? "60%" : "80%",
+              zIndex: 0,
+              pointerEvents: "none",
+            }}
+          />
+
           <p
             style={{
               fontSize: min ? "1.3rem" : "1.2rem",
@@ -58,59 +70,65 @@ class Cargando extends React.Component {
               fontStyle: "italic",
               lineHeight: min ? "1.4" : "1.3",
               textShadow: "0px 0px 8px rgba(0,0,0,0.6)",
+              position: "relative",
+              zIndex: 1,
             }}
           ></p>
 
           <a
             href="https://www.instagram.com/festivalkilometro/"
+            target="_blank"
             rel="noopener noreferrer"
             style={{
               display: "flex",
               justifyContent: "center",
               alignItems: "center",
+              position: "relative",
+              zIndex: 1,
             }}
           >
             <img
               src={botonIG}
               alt="Instagram"
               style={{
-                width: min ? "50px" : "45px",
-                filter: "drop-shadow(0px 0px 5px rgba(0,0,0,0.6))",
+                width: min ? "200px" : "150px",
+
               }}
             />
           </a>
+
           <a
             href="https://www.tiktok.com/@festival.km?_r=1&_t=ZS-94ldULst5ve"
+            target="_blank"
             rel="noopener noreferrer"
             style={{
               display: "flex",
               justifyContent: "center",
               alignItems: "center",
+              position: "relative",
+              zIndex: 1,
             }}
           >
             <img
               src={botontiktok}
-              alt="Instagram"
+              alt="TikTok"
               style={{
-                width: min ? "50px" : "45px",
-                filter: "drop-shadow(0px 0px 5px rgba(0,0,0,0.6))",
+                width: min ? "170px" : "130px",
+
               }}
             />
           </a>
         </div>
 
-        {/* Botón Entradas (oculto por ahora) */}
         <div
           style={{
             position: "absolute",
             ...(min
               ? {
-                  // Desktop: arriba a la derecha
                   top: "5%",
                   right: "3%",
                 }
               : {
-                  // Mobile: centrado debajo del IG
                   top: "86%",
                   left: "50%",
                   transform: "translateX(-50%)",
@@ -118,7 +136,7 @@ class Cargando extends React.Component {
           }}
         >
           {/* <a href="https://qlokura.tv/" target="_blank" rel="noopener noreferrer">
-            <img src={botonEntradas} alt="Comprar Entradas" style={{width: min ? '180px' : '200px'}} />
+            <img src={botonEntradas} alt="Comprar Entradas" style={{ width: min ? '180px' : '200px' }} />
           </a> */}
         </div>
       </div>
