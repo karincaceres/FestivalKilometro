dd9ff9aecd01787a8165eea22f9f997503d8fb2c
Karin <102821746+karincaceres@users.noreply.github.com>
2026-03-30 11:05:55 -0300

    Add files via upload

--- src/Pages/Cargando.js
+++ src/Pages/Cargando.js
@@ -3,22 +3,12 @@ import "./Main.css";
 
 import FondoMobile from "../assets/Cargando/Cargando-vertical.png";
 import FondoDesktop from "../assets/Cargando/Cargando-horizontal.png";
-// eslint-disable-next-line
-import BotonEntradas from "../assets/Cargando/bot_2.png";
-// eslint-disable-next-line
-import BotonEntradasPC from "../assets/Cargando/bot_2_pc.png";
-import BotonIG_cel from "../assets/Cargando/btn_ig.png";
-import BotonIG_pc from "../assets/Cargando/btn_ig.png";
-import botontiktok from "../assets/Cargando/tk.png";
-import backBotton from "../assets/Cargando/fondo_bot.png";
+import compras from "../assets/Varios/COMPRA.png";
 
 class Cargando extends React.Component {
   render() {
     const min = window.innerWidth >= 790;
     const backImg = min ? FondoDesktop : FondoMobile;
-    // eslint-disable-next-line
-    const botonEntradas = min ? BotonEntradasPC : BotonEntradas;
-    const botonIG = min ? BotonIG_pc : BotonIG_cel;
 
     return (
       <div
@@ -36,108 +26,31 @@ class Cargando extends React.Component {
           overflow: "hidden",
         }}
       >
+        {/* Botón Compras */}
         <div
           style={{
             position: "absolute",
-            bottom: min ? "20%" : "20%",
+            bottom: min ? "22%" : "25%",
             display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
             justifyContent: "center",
-            gap: min ? "20px" : "8px",
-            textAlign: "center",
-            width: min ? "900px" : "390px",
+            alignItems: "center",
+            zIndex: 2,
           }}
         >
-          <img
-            src={backBotton}
-            alt=""
-            style={{
-              position: "absolute",
-              width: min ? "60%" : "80%",
-              zIndex: 0,
-              pointerEvents: "none",
-            }}
-          />
-
-          <p
-            style={{
-              fontSize: min ? "1.3rem" : "1.2rem",
-              fontWeight: 600,
-              color: "#fff",
-              fontFamily: "FuturaHvBTHeavy",
-              margin: 0,
-              fontStyle: "italic",
-              lineHeight: min ? "1.4" : "1.3",
-              textShadow: "0px 0px 8px rgba(0,0,0,0.6)",
-              position: "relative",
-              zIndex: 1,
-            }}
-          ></p>
-
           <a
-            href="https://www.instagram.com/festivalkilometro/"
+            href="https://www.edenentradas.ar/event/festival-kilometro-wh"
             target="_blank"
             rel="noopener noreferrer"
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              position: "relative",
-              zIndex: 1,
-            }}
           >
             <img
-              src={botonIG}
-              alt="Instagram"
+              src={compras}
+              alt="Comprar Entradas"
               style={{
-                width: min ? "200px" : "150px",
-
+                width: min ? "420px" : "300px",
+                display: "block",
               }}
             />
           </a>
-
-          <a
-            href="https://www.tiktok.com/@festival.km?_r=1&_t=ZS-94ldULst5ve"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              position: "relative",
-              zIndex: 1,
-            }}
-          >
-            <img
-              src={botontiktok}
-              alt="TikTok"
-              style={{
-                width: min ? "170px" : "130px",
-
-              }}
-            />
-          </a>
-        </div>
-
-        <div
-          style={{
-            position: "absolute",
-            ...(min
-              ? {
-                  top: "5%",
-                  right: "3%",
-                }
-              : {
-                  top: "86%",
-                  left: "50%",
-                  transform: "translateX(-50%)",
-                }),
-          }}
-        >
-          {/* <a href="https://qlokura.tv/" target="_blank" rel="noopener noreferrer">
-            <img src={botonEntradas} alt="Comprar Entradas" style={{ width: min ? '180px' : '200px' }} />
-          </a> */}
         </div>
       </div>
     );
