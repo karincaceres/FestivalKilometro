6e8179fcfea5dfd67f4c70edf81ed2aef1b1c192
Karin <102821746+karincaceres@users.noreply.github.com>
2026-04-14 13:18:06 -0300

    Add files via upload

--- src/Pages/Fiesta.js
+++ src/Pages/Fiesta.js
@@ -2,15 +2,15 @@ import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import "./Main.css";
 // eslint-disable-next-line
-import slide1 from "../assets/Slider/slide-1.png";
+import slide1 from "../assets/Slider/1920 x 600/1_1920x600px.png";
 // eslint-disable-next-line
-import slide2 from "../assets/Slider/slide-2.png";
-import slide3 from "../assets/Slider/slide-3.png";
-import mobileSlide1 from "../assets/Slider/mobile-slide-1.png";
-import mobileSlide2 from "../assets/Slider/mobile-slide-2.png";
-import mobileSlide3 from "../assets/Slider/mobile-slide-3.png";
-import mobileSlide4 from "../assets/Slider/mobile-slide-4.png";
-import mobileSlide5 from "../assets/Slider/mobile-slide-5.png";
+import slide2 from "../assets/Slider/1920 x 600/2_1920x600px.png";
+import slide3 from "../assets/Slider/1920 x 600/3_1920x600px.png";
+import mobileSlide1 from "../assets/Slider/500 x 500/1_500x500px.png";
+import mobileSlide2 from "../assets/Slider/500 x 500/2_500x500px.png";
+import mobileSlide3 from "../assets/Slider/500 x 500/3_500x500px.png";
+import mobileSlide4 from "../assets/Slider/500 x 500/4_500x500px.png";
+import mobileSlide5 from "../assets/Slider/500 x 500/5_500x500px.png";
 
 import background_h from "../assets/Fondos/fondo1.png";
 import background_v from "../assets/Fondos/fondoV.png";
@@ -88,7 +88,7 @@ class Fiesta extends React.Component {
             <iframe
               width="100%"
               height="100%"
-            src="https://www.youtube.com/embed/QX0hToDbFn4"  
+            src="https://www.youtube.com/embed/QX0hToDbFn4"
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
--- src/Pages/SliderHome.js
+++ src/Pages/SliderHome.js
@@ -2,15 +2,20 @@ import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import "./Main.css";
 
-import slide1 from "../assets/Slider/slide-1.png";
-import slide2 from "../assets/Slider/slide-2.png";
-import slide3 from "../assets/Slider/slide-3.png";
-import mobileSlide1 from "../assets/Slider/mobile-slide-1.png";
-import mobileSlide2 from "../assets/Slider/mobile-slide-2.png";
-import mobileSlide3 from "../assets/Slider/mobile-slide-3.png";
-import mobileSlide4 from "../assets/Slider/mobile-slide-4.png";
-import mobileSlide5 from "../assets/Slider/mobile-slide-5.png";
-
+import slide1 from "../assets/Slider/1920 x 600/1_1920x600px.png";
+import slide2 from "../assets/Slider/1920 x 600/2_1920x600px.png";
+import slide3 from "../assets/Slider/1920 x 600/3_1920x600px.png";
+import slide4 from "../assets/Slider/1920 x 600/4_1920x600px.png";
+import mobileSlide1 from "../assets/Slider/500 x 500/1_500x500px.png";
+import mobileSlide2 from "../assets/Slider/500 x 500/2_500x500px.png";
+import mobileSlide3 from "../assets/Slider/500 x 500/3_500x500px.png";
+import mobileSlide4 from "../assets/Slider/500 x 500/4_500x500px.png";
+import mobileSlide5 from "../assets/Slider/500 x 500/5_500x500px.png";
+import mobileSlide6 from "../assets/Slider/500 x 500/6_500x500px.png";
+import mobileSlide7 from "../assets/Slider/500 x 500/7_500x500px.png";
+import mobileSlide8 from "../assets/Slider/500 x 500/8_500x500px.png";
+import mobileSlide9 from "../assets/Slider/500 x 500/9_500x500px.png";
+import mobileSlide10 from "../assets/Slider/500 x 500/10_500x500px.png";
 import background_h from "../assets/Fondos/fondo1.png"
 import background_v from "../assets/Fondos/fondoV.png"
 
@@ -22,8 +27,19 @@ class SliderHome extends React.Component {
     const footerHeight = isMobile ? 160 : 70;
     const sliderHeight = `calc(100vh - ${headerHeight + footerHeight}px)`;
 
-    const desktopSlides = [slide1, slide2, slide3];
-    const mobileSlides = [mobileSlide1, mobileSlide2, mobileSlide3, mobileSlide4, mobileSlide5];
+    const desktopSlides = [slide1, slide2, slide3, slide4];
+    const mobileSlides = [
+      mobileSlide1,
+      mobileSlide2,
+      mobileSlide3,
+      mobileSlide4,
+      mobileSlide5,
+      mobileSlide6,
+      mobileSlide7,
+      mobileSlide8,
+      mobileSlide9,
+      mobileSlide10
+    ];
     const slides = isMobile ? mobileSlides : desktopSlides;
  	const Back = isMobile ? background_v : background_h;
     return (
@@ -34,7 +50,7 @@ class SliderHome extends React.Component {
           indicators={isMobile}
           controls={!isMobile}
           interval={4000}
-          style={{ height: "100%" }}
+          style={{ height: "70%" }}
         >
           {slides.map((src, i) => (
             <Carousel.Item key={i} style={{ height: sliderHeight }}>
