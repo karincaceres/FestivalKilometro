6a901143f7764b29201d277ad149975af80d95c4
Karin <102821746+karincaceres@users.noreply.github.com>
2026-05-04 11:54:07 -0300

    Add files via upload

--- src/Pages/Cargando.js
+++ src/Pages/Cargando.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./Main.css";
 
-import FondoMobile from "../assets/Cargando/Cargando-vertical.png";
-import FondoDesktop from "../assets/Cargando/Cargando-horizontal.png";
-import compras from "../assets/Varios/COMPRA.png";
+import FondoMobile from "../assets/Cargando/Cierre_provisorio_1Mobile.png";
+import FondoDesktop from "../assets/Cargando/Cierre_provisorio_1920x600px.png";
+import compras from "../assets/Cargando/Bot_gacetilla.png";
 
 class Cargando extends React.Component {
   render() {
@@ -30,7 +30,7 @@ class Cargando extends React.Component {
         <div
           style={{
             position: "absolute",
-            bottom: min ? "22%" : "25%",
+            bottom: min ? "12%" : "15%",
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
@@ -38,7 +38,7 @@ class Cargando extends React.Component {
           }}
         >
           <a
-            href="https://www.edenentradas.ar/event/festival-kilometro-wh"
+            href="https://drive.google.com/file/d/1xKF1nwRBrHRwdcxWESr9DtKoqqIeoohE/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -46,7 +46,7 @@ class Cargando extends React.Component {
               src={compras}
               alt="Comprar Entradas"
               style={{
-                width: min ? "420px" : "300px",
+                width: min ? "620px" : "370px",
                 display: "block",
               }}
             />
